7436b752b9624e8fef597de4a57fd4f9ca2bf8f6
MahdiGolmohamadii <mmahdigolmohamadi@gmail.com>
2025-09-15 18:30:05 -0400

    inital commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+from routers import user
+
+app = FastAPI()
+
+app.include_router(user.router)
+
+
+@app.get("/")
+async def root():
+    return{"message": "we are live!"}
